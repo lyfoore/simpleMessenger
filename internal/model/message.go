c7e326a7cbcdf9bfcb39c3099580c30b6af7d724
lyfoore <lyfoore@gmail.com>
2026-02-13 00:09:11 +0700

    Add text field to the message struct

--- internal/model/message.go
+++ internal/model/message.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Message struct {
 	gorm.Model
-	ChatID uint `gorm:"column:chat_id; not null" json:"chatId"`
-	UserID uint `gorm:"column:user_id; not null" json:"userId"`
+	Text   string `gorm:"column:text; not null" json:"text"`
+	ChatID uint   `gorm:"column:chat_id; not null" json:"chatId"`
+	UserID uint   `gorm:"column:user_id; not null" json:"userId"`
 }
